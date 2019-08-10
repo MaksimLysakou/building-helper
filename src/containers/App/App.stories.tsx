97da5c4c6a4075f4da5a5486812661f771c2af62
Maksim Lysakou <maksim.lysakou@gmail.com>
2019-08-10 17:42:49 +0300

    Remove unused story

--- src/containers/App/App.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import * as React from 'react';
-storiesOf("Button", module)
-  .add("with text", () => (
-    <button>Hello Button</button>
-  ))
-  .add("with some emoji", () => (
-    <button>😀 😎 👍 💯</button>
-  ));
